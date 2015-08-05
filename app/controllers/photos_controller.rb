6c0fd19ac8ba853ee06feb523397fec303f2c08b
Andi Fuess <afuess@chicagobooth.edu>
2015-08-04 20:13:41 -0500

    commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,19 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+@photo_id=params["id"]
+@photo_source=Photo.find(@photo_id).source
+@photo_caption=Photo.find(@photo_id).caption
+render ("show.html.erb")
+end
+
+def new_form
+render("new_form.html.erb")
+end
+
+def create_photo
+  render("create_photo.html.erb")
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
-
-  # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
-
+  get("/create_photo", { :controller => "photos", :action => "create_photo" })
 end
