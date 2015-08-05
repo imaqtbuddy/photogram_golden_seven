c71bdf21fda3a4ea23ebfec44de2f41149438eed
Andi Fuess <afuess@chicagobooth.edu>
2015-08-04 21:08:50 -0500

    commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,19 +3,46 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
-def show
-@photo_id=params["id"]
-@photo_source=Photo.find(@photo_id).source
-@photo_caption=Photo.find(@photo_id).caption
-render ("show.html.erb")
-end
+  def show
+    @photo_id=params["id"]
+    @photo_source=Photo.find(@photo_id).source
+    @photo_caption=Photo.find(@photo_id).caption
+    render ("show.html.erb")
+  end
 
-def new_form
-render("new_form.html.erb")
-end
+  def new_form
+    render("new_form.html.erb")
+  end
 
-def create_photo
-  render("create_photo.html.erb")
-end
+  def create_photo
+    i=Photo.new
+    i.source=params["the_source"]
+    i.caption=params["the_caption"]
+    i.save
+    redirect_to("http://localhost:3000/photos")
+  end
 
-end
+  def destroy
+    @photo_id=params["id"]
+    i = Photo.find(@photo_id)
+    i. destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo_id=params["id"]
+    @photo_source=Photo.find(@photo_id).source
+    @photo_caption=Photo.find(@photo_id).caption
+    render("edit_form.html.erb")
+  end
+
+  def update_photo
+    @photo_id=params["id"]
+    i = Photo.find(@photo_id)
+    i.source=params["the_source"]
+    i.caption=params["the_caption"]
+    i. save
+    redirect_to action:"show"
+  end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get("/photos/new", { :controller => "photos", :action => "new_form" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
   get("/create_photo", { :controller => "photos", :action => "create_photo" })
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_photo" })
 end
